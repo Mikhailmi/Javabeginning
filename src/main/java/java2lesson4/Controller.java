e796a9f3b80c8dcb8c7437b56c31be83611dfe8d
Mikhail <Mi1995@yandex.ru>
2021-11-02 14:19:34 +0300

    Первая домашка второго курса. Не справил с четвертым заданием (см. комментарий в нем).

---
+++ src/main/java/java2lesson4/Controller.java
@@ -0,0 +1,20 @@
+package java2lesson4;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+public class Controller {
+    @FXML
+    TextArea mainTextArea;
+    @FXML
+    TextField enterTextField;
+
+
+
+    public void btnOneClickAction(ActionEvent actionEvent) {
+                mainTextArea.setText(enterTextField.getText());
+                enterTextField.setText("");
+    }
+
+}
